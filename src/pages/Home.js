d524787bb0fdace5ac28cebcc160fe9525110fa3
shrivastavaritesh807 <shrivastavaritesh807@gmail.com>
2024-10-16 12:59:32 +0530

    test2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,14 @@ import {Box} from '@mui/material';
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
+//  dqwefqefweqfeqw
 const Home = () => {
     return(
         <>
          <Box>
              <HeroBanner/>
              <SearchExercises/>
+             <h2>wefqwefqwe</h2>
              <Exercises/>
          </Box>
         </>
